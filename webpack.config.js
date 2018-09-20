f391793b5352934f92db52ee857a186481b10654
Miguel Malagon <miguelmalagon@outlook.com>
2018-09-20 13:44:42 -0500

    use absolute paths

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
 const path = require('path');
 module.exports = {// add babel polyfill to run most new code on js
-  entry: ['babel-polyfill','./lib/components/index.js'],
+  resolve: {
+    modules:[
+      path.resolve('./lib'),
+      path.resolve('./node_modules'),
+    ]
+  },
+  entry: ['babel-polyfill','./lib/renderers/dom.js'],
   output: {
     path: path.resolve(__dirname,'public'),
     filename: 'bundle.js'
